fba919514b6b1243b6039c2eefc1af195da8eb7f
djyuning <djyuning@126.com>
2018-08-30 11:53:16 +0800

    分离视图文件；

--- lib/main.dart
+++ lib/main.dart
@@ -1,107 +1,29 @@
 import 'package:flutter/material.dart';
 
+import './pages/HomePage.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      // 该标题会显示在任务列表中
       title: 'Flutter Demo',
+
+      // 主题配置
       theme: new ThemeData(
         primarySwatch: Colors.red,
       ),
+
+      // 不显示右上角的 DEBUG 标签
       debugShowCheckedModeBanner: false,
-      home: new MyHomePage(),
-    );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+      // 指定主页
+      home: new HomePage(),
 
-  final String title;
+      // 路由配置
 
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Home'),
-        actions: <Widget>[
-          new IconButton(icon: new Icon(Icons.person), onPressed: () => {}),
-          new IconButton(icon: new Icon(Icons.search), onPressed: () => {}),
-        ],
-      ),
-      body: new Center(
-        child: new SingleChildScrollView(
-          child: new Column(
-            children: <Widget>[
-              new Image.asset('images/demo2.jpg'),
-              new Image.asset('images/demo2.jpg'),
-              new Image.asset('images/demo2.jpg'),
-              new Image.network(
-                  'https://i2.hdslb.com/bfs/face/7274533b300efb5110aaabdedcc8871c1cd45619.jpg@72w_72h.webp'),
-              new Text(
-                'You have pushed the button this many times:',
-              ),
-              new Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.display1,
-              ),
-            ],
-          ),
-        ),
-      ),
-      drawer: new Drawer(
-        child: new Column(
-          children: <Widget>[
-            new Image.asset('images/demo2.jpg'),
-          ],
-        ),
-      ),
-      bottomNavigationBar: new BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          new BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text('Home'),
-          ),
-          new BottomNavigationBarItem(
-            icon: new Icon(Icons.rss_feed),
-            title: new Text('Home'),
-          ),
-          new BottomNavigationBarItem(
-            icon: new Icon(Icons.local_offer),
-            title: new Text('Home'),
-          ),
-          new BottomNavigationBarItem(
-            icon: new Icon(Icons.explore),
-            title: new Text('Home'),
-          ),
-          new BottomNavigationBarItem(
-            icon: new Icon(Icons.person),
-            title: new Text('Home'),
-          ),
-        ],
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tp_fl_one/main.dart';
+import 'package:FlutterApp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
